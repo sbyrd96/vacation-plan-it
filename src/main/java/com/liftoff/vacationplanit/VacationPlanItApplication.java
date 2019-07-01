8605f3891f2f91eac66d4293b54d090f63e168f5
Susan Byrd <byrd96@outlook.com>
2019-06-30 23:07:10 -0500

    added "add" page and link to add

---
+++ src/main/java/com/liftoff/vacationplanit/VacationPlanItApplication.java
@@ -0,0 +1,13 @@
+package com.liftoff.vacationplanit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VacationPlanItApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VacationPlanItApplication.class, args);
+	}
+
+}
